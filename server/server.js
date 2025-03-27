169708686ab40f4201d9ae8d7dff7bd5b99e18b6
aya-asylbek <aichurek0990@gmail.com>
2025-03-26 23:49:46 -0700

    Added post query to add a new contact

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import db from "./db.js";
 config();
 
 const app = express();
-const PORT = process.env.PORT|| 5001;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(json());
@@ -26,23 +26,35 @@ app.get('/contacts', async (req, res) => {
         res.status(500).json({ error: 'Internal server error' });
     }
 });
-  
+
+
+//Get only bi id 
 app.get('/contacts/:id', async (req, res) => {
     try {
-        const contactId =req.params.id;
-        const contact = await db.oneOrNone('SELECT * FROM contacts WHERE id = $1',
-            [contactId]
-        );
-       
-        if (!contact) {
-            return res.status(404).json({ error: "Contact not found" });
-        }
+        const contact = await db.oneOrNone('SELECT * FROM contacts WHERE id = $1', [req.params.id]);
+        if (!contact) return res.status(404).json({ error: 'Contact not found' });
         res.json(contact);
     } catch (err) {
-        console.error('Error fetching contacts:', err);
+        console.error('Error fetching contact by ID:', err);
         res.status(500).json({ error: 'Internal server error' });
     }
 });
+
+//ad a new contact
+app.post('/contacts', async (req, res) => {
+    const { name, email, phone, notes } = req.body;
+    try {
+        const newContact = await db.one(
+            'INSERT INTO contacts (name, email, phone, notes) VALUES ($1, $2, $3, $4) RETURNING *',
+            [name, email, phone, notes]
+        );
+        res.status(201).json(newContact);
+    } catch (err) {
+        console.error('Error adding contact:', err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
 
 
 
